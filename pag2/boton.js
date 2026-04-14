dbfa0dcb448ee3ffc9f99f1d62b28d4cf3370778
Kevin Escorcia <165961257+KevinLise@users.noreply.github.com>
2026-04-14 14:04:54 -0500

    Add files via upload

---
+++ pag2/boton.js
@@ -0,0 +1,4 @@
+function atras() {
+    window.location.href = "enemigos.html";
+}
+document.getElementById("boton").addEventListener("click", atras);
---
+++ pag2/boton2.js
@@ -0,0 +1,4 @@
+function adelante() {
+    window.location.href = "index.html";
+}
+document.getElementById("boton_2").addEventListener("click", adelante);
